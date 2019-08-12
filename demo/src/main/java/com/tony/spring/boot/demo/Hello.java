31497a216edb4c7e72cde007650e1ce74543d66c
welfly <1085597986@qq.com>
2019-08-12 21:25:22 +0800

    修改测试

--- demo/src/main/java/com/tony/spring/boot/demo/Hello.java
+++ demo/src/main/java/com/tony/spring/boot/demo/Hello.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
 	@RequestMapping(value="/say3", method=RequestMethod.GET)
 	public String sayHi() {
-		return "Hello SpringBoot33333!";
+		return "Hello SpringB345oot33333!";
 	}
 }
